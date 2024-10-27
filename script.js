03b95476f2b5bacf0fedde9b4608b4d9d70b2473
nicoyestra <nicoyestra@gmail.com>
2024-10-26 22:34:38 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Cambiar de imagen automáticamente cada 5 segundos
+setInterval(nextSlide, 5000);
+
+// Mostrar la primera diapositiva
+showSlide(currentSlide);
